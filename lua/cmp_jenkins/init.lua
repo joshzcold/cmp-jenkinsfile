4d3b1ca65858382bd604bea6558215814ef9bf6b
Nicholas <kressnick25@gmail.com>
2025-05-11 20:57:43 +1000

    fix validation descriptions

--- lua/cmp_jenkins/init.lua
+++ lua/cmp_jenkins/init.lua
@@ -47,10 +47,10 @@ function source:complete(params, callback)
   vim.validate('gdsl_file', params.option.gdsl_file, 'string', false, '`opts.gdsl_file` must be `string`')
   vim.validate('jenkins_url', params.option.jenkins_url, 'string', false, '`opts.jenkins_url` must be `string`')
   vim.validate('http', params.option.http, 'table', false, '`opts.http` must be `table`')
-  vim.validate('http.basic_auth_user', params.option.http.basic_auth_user, 'string', false, '`http.basic_auth_user` must be `string`')
-  vim.validate('http.basic_auth_password', params.option.http.basic_auth_password, 'string', false, '`http.basic_auth_password` must be `string`')
-  vim.validate('http.ca_cert', params.option.http.ca_cert, 'string', false, '`http.ca_cert` must be `string`')
-  vim.validate('http.proxy', params.option.http.proxy, 'string', false, '`http.proxy` must be `string`')
+  vim.validate('http.basic_auth_user', params.option.http.basic_auth_user, 'string', false, '`opts.http.basic_auth_user` must be `string`')
+  vim.validate('http.basic_auth_password', params.option.http.basic_auth_password, 'string', false, '`opts.http.basic_auth_password` must be `string`')
+  vim.validate('http.ca_cert', params.option.http.ca_cert, 'string', false, '`opts.http.ca_cert` must be `string`')
+  vim.validate('http.proxy', params.option.http.proxy, 'string', false, '`opts.http.proxy` must be `string`')
 
   if params.option.jenkins_url ~= "" then
     if not file_exists(params.option.gdsl_file) or file_is_empty(params.option.gdsl_file) then
